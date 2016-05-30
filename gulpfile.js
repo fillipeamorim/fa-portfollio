6114c4e9c7c56528f7da78d380fa94ffb9267e39
Fillipe Amorim <fillipeamorim@me.com>
2016-05-30 13:05:32 -0300

    Updated project configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,86 @@
-var jade         = require('jade'),
-    browserSync  = require('browser-sync');
-    runSequence  = require('run-sequence');
-    gulp         = require('gulp'),
-    gulpLess         = require('gulp-less'),
-    imagemin     = require('gulp-imagemin'),
-    cache        = require('gulp-cache'),
-    uglify       = require('gulp-uglify'),
-    autoprefixer = require('gulp-autoprefixer'),
+// Gulp File
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssBase64 = require('gulp-css-base64'),
+    path = require('path'),
+    notify = require('gulp-notify'),
     inlinesource = require('gulp-inline-source'),
-    watch        = require('gulp-watch'),
-    notify       = require('gulp-notify'),
-    gulpJade     = require('gulp-jade'),
-    gulpWatch    = require('gulp-watch'),
+    browserSync = require('browser-sync'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
+    cache = require('gulp-cache'),
+    uglify = require('gulp-uglify'),
+    autoprefixer = require('gulp-autoprefixer'),
+    runSequence = require('run-sequence');
 
-gulp.task('jade', function() {
-  return gulp.src('./src/*.jade')
-    .pipe(gulpJade({
-      jade: jade,
-      pretty: true
-    }))
+// Task to compile SCSS
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: false,
+      paths: [ path.join(__dirname, 'scss', 'includes') ]
+    })
+    .on("error", notify.onError(function(error) {
+      return "Failed to Compile SCSS: " + error.message;
+    })))
+    .pipe(cssBase64())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./src/'))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+    .pipe(notify("SCSS Compiled Successfully :)"));
+});
+
+gulp.task('styleguide', function () {
+  return gulp.src('./src/scss/styleguide.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    errLogToConsole: false,
+    paths: [ path.join(__dirname, 'scss', 'includes') ]
+  })
+  .on("error", notify.onError(function(error) {
+    return "Failed to Compile Styleguide SCSS: " + error.message;
+  })))
+  .pipe(cssBase64())
+  .pipe(autoprefixer())
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./src/css/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+  .pipe(notify("Styleguide SCSS Compiled Successfully :)"));
+});
+
+// Task to process jade
+gulp.task('jadefy', function() {
+  return gulp.src('./src/*.jade')
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest('./src/'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+// Task to Minify JS
+gulp.task('jsmin', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+// Minify Images
+gulp.task('imagemin', function (){
+  return gulp.src('./src/img/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('./dist/img'));
 });
 
 // BrowserSync Task (Live reload)
@@ -31,11 +92,32 @@ gulp.task('browserSync', function() {
   })
 });
 
+// Gulp Inline Source Task
+// Embed scripts, CSS or images inline (make sure to add an inline attribute to the linked files)
+// Eg: <script src="default.js" inline></script>
+// Will compile all inline within the html file (less http requests - woot!)
+gulp.task('inlinesource', function () {
+  return gulp.src('./src/**/*.html')
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./dist/'));
+});
+
+// Gulp Watch Task
 gulp.task('watch', ['browserSync'], function () {
-   gulp.watch('./src/**/*.jade', ['jade']);
+   gulp.watch('./src/scss/**/*', ['sass', 'styleguide']);
+   gulp.watch('./src/**/*.jade', ['jadefy']);
    gulp.watch('./src/**/*.html').on('change', browserSync.reload);
-   gulp.watch('./src/css/**/*.css').on('change', browserSync.reload);
+});
+
+// Gulp Clean Up Task
+gulp.task('clean', function() {
+  del('dist');
 });
 
 // Gulp Default Task
 gulp.task('default', ['watch']);
+
+// Gulp Build Task
+gulp.task('build', function() {
+  runSequence('clean', 'sass', 'styleguide', 'imagemin', 'jsmin', 'inlinesource');
+});
